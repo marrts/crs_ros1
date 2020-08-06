f966c891a8974e47ef41051ca47d3fc40b271c3e
Tyler Marr <tyler.marr@swri.org>
2020-08-06 17:49:37 -0500

    Moved controller changer to robot comms, added io settings

--- crs_motion_execution/src/compliance_trajectory_server.cpp
+++ crs_motion_execution/src/compliance_trajectory_server.cpp
@@ -24,11 +24,6 @@ static const std::string WRENCH_TOPIC = "/wrench";
 static const std::string SANDER_WRENCH_TOPIC = "/sander_wrench";
 static const std::string SANDER_SPEED_TOPIC = "/sander_speed";
 static const std::string ZERO_FTSENSOR_SERVICE = "/ur_hardware_interface/zero_ftsensor";
-static const std::string SWITCH_CONTROLLER_TOPIC = "/controller_manager/switch_controller";
-static const std::string SWITCH_CONTROLLER_SERVICE = "compliance_controller_on";
-static const std::string COMPLIANCE_CONTROLLER_NAME = "pos_compliance_controller";
-static const std::string JOINT_CONTROLLER_NAME = "scaled_pos_traj_controller";
-static const std::string GET_ACTIVE_CONTROLLERS_SERVICE = "/controller_manager/list_controllers";
 
 bool checkTolerance(const geometry_msgs::Vector3& error, const geometry_msgs::Vector3& tolerance)
 {
@@ -49,99 +44,13 @@ public:
     speed_pub_= nh.advertise<std_msgs::Float64>(SANDER_SPEED_TOPIC , 10);
     wrench_sub_ = nh.subscribe<geometry_msgs::WrenchStamped>(WRENCH_TOPIC, 10, &CartAction::wrenchCB, this);
 
-    set_controller_service_ = nh.advertiseService(SWITCH_CONTROLLER_SERVICE, &CartAction::switchControllerCB, this);
-
-    controller_manager_client_ = nh.serviceClient<controller_manager_msgs::SwitchController>(SWITCH_CONTROLLER_TOPIC);
     zero_ftsensor_client_ = nh.serviceClient<std_srvs::Trigger>(ZERO_FTSENSOR_SERVICE);
-    list_controller_client_ = nh.serviceClient<controller_manager_msgs::ListControllers>(GET_ACTIVE_CONTROLLERS_SERVICE);
 
     as_.start();
 
   }
 
 protected:
-
-  bool switchControllerCB(std_srvs::SetBool::Request  &req,
-                          std_srvs::SetBool::Response &res)
-  {
-    std::vector<std::string> start_controllers, stop_controllers;
-    controller_manager_msgs::ListControllers list_ctrl_request;
-    if(!list_controller_client_.waitForExistence(ros::Duration(3)))
-    {
-      ROS_ERROR("Controller manager not found");
-      res.message = "Controller manager not found";
-      res.success = false;
-      return false;
-    }
-    list_controller_client_.call(list_ctrl_request);
-    bool joint_active = false, cart_active = false;
-    for (auto ctrl : list_ctrl_request.response.controller)
-    {
-      if(ctrl.name.compare(JOINT_CONTROLLER_NAME) == 0 && ctrl.state.compare("running") == 0)
-        joint_active = true;
-      else if(ctrl.name.compare(COMPLIANCE_CONTROLLER_NAME) == 0 && ctrl.state.compare("running") == 0)
-        cart_active = true;
-    }
-    if (req.data) // Set to cartesian controller
-    {
-      if (!joint_active && cart_active)
-      {
-        ROS_WARN("Cartesian controller already active");
-        res.message = "Cartesian controller already active";
-        res.success = true;
-        return true;
-      }
-      ROS_WARN("Activating Cartesian control on the robot");
-      if (!cart_active)
-        start_controllers.push_back(COMPLIANCE_CONTROLLER_NAME);
-      if (joint_active)
-        stop_controllers.push_back(JOINT_CONTROLLER_NAME);
-    }
-    else // Set to joint controller
-    {
-      if (joint_active && !cart_active)
-      {
-        ROS_WARN("Joint controller already active");
-        res.message = "Joint controller already active";
-        res.success = true;
-        return true;
-      }
-      ROS_WARN("Activating joint control on the robot");
-      if (!joint_active)
-        start_controllers.push_back(JOINT_CONTROLLER_NAME);
-      if (cart_active)
-        stop_controllers.push_back(COMPLIANCE_CONTROLLER_NAME);
-    }
-    controller_manager_msgs::SwitchController change_ctrl_request;
-    change_ctrl_request.request.start_controllers = start_controllers;
-    change_ctrl_request.request.stop_controllers = stop_controllers;
-    change_ctrl_request.request.strictness = controller_manager_msgs::SwitchController::Request::STRICT;
-    change_ctrl_request.request.start_asap = true;
-    change_ctrl_request.request.timeout = 1.0;
-    if(!controller_manager_client_.waitForExistence(ros::Duration(3)))
-    {
-      ROS_ERROR("Controller manager not found");
-      res.message = "Controller manager not found";
-      res.success = false;
-      return false;
-    }
-    controller_manager_client_.call(change_ctrl_request);
-
-    std::cout << "Done" << std::endl;
-    if (change_ctrl_request.response.ok)
-    {
-      res.message = "Controller successfully updated";
-      res.success = true;
-      return true;
-    }
-    else
-    {
-      ROS_ERROR("Controller failed to update");
-      res.message = "Controller failed to update";
-      res.success = false;
-      return false;
-    }
-  }
 
   void executeCB(const cartesian_trajectory_msgs::CartesianComplianceTrajectoryGoalConstPtr &goal)
   {
@@ -410,8 +319,7 @@ protected:
   actionlib::SimpleActionServer<cartesian_trajectory_msgs::CartesianComplianceTrajectoryAction> as_; // NodeHandle instance must be created before this line. Otherwise strange error occurs.
   ros::Publisher target_wrench_pub_, target_frame_pub_, curr_sander_wrench_, speed_pub_;
   ros::Subscriber wrench_sub_;
-  ros::ServiceServer set_controller_service_;
-  ros::ServiceClient controller_manager_client_, zero_ftsensor_client_, list_controller_client_;
+  ros::ServiceClient  zero_ftsensor_client_;
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener listener_;
   std::string action_name_;
